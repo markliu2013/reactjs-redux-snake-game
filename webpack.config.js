2b02aec10ea8c29ee7583b01cb3c97e120de6cac
markliu2013 <markliu2013@gmail.com>
2019-07-21 14:36:59 +0800

    Infrastructure

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+import path from 'path';
+
+module.exports = {
+    mode: 'development',
+    entry: [ path.resolve(__dirname, 'src', 'index.js') ],
+    output: {
+        path: '/static/',
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+        ]
+    }
+}
+
