1480574e56e1dfc8cf538efac886f4ae62e91a8a
markliu2013 <markliu2013@gmail.com>
2022-11-13 19:39:17 +0800

    save

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GameInfo = () => (
     <div className="gameInfo">
         <p>Tips：keep Pressing the arrow key, snake will keep moving.</p>
-        <p>Written by ZfwHub <a href="http://www.zfwhub.com/" target="_blank">http://www.zfwhub.com/</a></p>
+        <p>Written by Mark Liu <a href="https://github.com/markliu2013/reactjs-redux-snake-game/" target="_blank">https://github.com/markliu2013/reactjs-redux-snake-game/</a></p>
     </div>
 );
 
